2da6f8734d5c9f9d35fadbd89f2d5399569ff5da
aloktripathi1 <aloktripathe@gmail.com>
2025-05-29 10:34:37 +0530

    adding java week1 coursework to java in iitm-bs

---
+++ WeatherApp.java
@@ -0,0 +1,44 @@
+// public class WeatherApp {
+//     public static void main(String[] args) {
+        
+//         boolean isRaining = false;
+
+//         if (isRaining) {
+//             System.out.println("Its raining, Don't forget to bring Umbrella!");
+//         }
+//         else {
+//             System.out.println("Its not raining, Enjoy the sunshine.");
+//         }
+
+//         int temprature = 5;
+
+//         if (temprature < 35) {
+//             System.out.println("Its so cold, wear your jackets.");
+//         } 
+//         else if(temprature > 35){
+//             System.out.println("Its schorching hot, stay hydrated.");
+//         }
+//         else{
+//             System.out.println("It's warm and pleasant. A t-shirt is good.");
+//         }
+//     }
+// }
+
+
+public class WeatherApp{
+    public static void main(String[] args) {
+        // counting sheep from 1 to 5
+        for (int = 1; i<=5; i++) {
+            System.out.println("sheep number " + i + "...");
+        }
+        System.out.println("Zzzzz....")
+
+        System.out.println("\n----Countdown!----");
+
+        // counting down from 3 to 1
+        for (int count = 3; count >= 1; count--){
+            System.out.println(count);
+        }
+        System.out.println("Lift-off!");
+    }
+}
---
+++ Week1/Main.java
@@ -0,0 +1,31 @@
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+        
+//         // Calculate the area of the rectange
+
+//         double width = 0;
+//         double height = 0;
+//         double area = 0;
+
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter the width: ");
+
+//         width = scanner.nextDouble();
+
+//         System.out.println("Enter the height: ");
+//         height = scanner.nextDouble();
+
+//         area = width * height;
+
+//         System.out.println("The area of rectangle is " + area + "m²");
+        
+
+
+
+
+//         scanner.close();
+//     }
+// }
